ba694c520f8e5f8d0942c56cf3a8c751fba6bb2a
Maciek99 <64065294+Maciek99@users.noreply.github.com>
2024-04-05 23:58:49 +0200

    Update footer layout and add educational message

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,9 +40,15 @@ export default function RootLayout({
 
 export const Footer = () => {
   return (
-    <footer className="flex flex-col gap-5 border rounded-md bg-card px-5 py-2">
+    <footer className="flex flex-wrap justify-between gap-2 border rounded-md bg-card px-5 py-2">
       <p className="text-muted-foreground">
         Nowa strona: <Link className="text-green-500" href="https://github.com/Maciek99">Maciej Fidler</Link>, Orginalna Strona: Krzysztof Markowsk, Logo szkoły: Marcel Szczepek
+      </p>
+      <p className="text-muted-foreground">
+        Strona stworzona w celach edukacyjnych.
+      </p>
+      <p className="text-muted-foreground">
+        Znalazłeś błąd? Zgłoś go na <Link className="text-green-500" href="https://github.com/Maciek99/sp1ozarow-reimagined">GitHub</Link>
       </p>
     </footer>
 
